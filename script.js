f98cf988c1f807ab3ec8cf782bcbe402c58ea8e9
AgentMatthy <csaki.mateki@gmail.com>
2024-02-01 21:11:30 +0100

    a lot

---
+++ script.js
@@ -0,0 +1,8 @@
+var object = document.getElementById('twrdscursor');
+
+// Add event listener to track mouse movement
+document.addEventListener('mousemove', function(event) {
+    // Get mouse position
+    var height = event.clientX;
+    var width = event.clientX;
+});
